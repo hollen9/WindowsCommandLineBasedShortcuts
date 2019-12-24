cd1e1b70390853fd1ca43de95bde773d0ff17efd
hollen9 <hollen.tw@gmail.com>
2019-12-24 16:17:52 +0800

    Add project files.

---
+++ ChromeRemoteDesktopChromeAppMode/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace ChromeRemoteDesktopChromeAppMode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Process.Start("chrome.exe", "--app=\"https://remotedesktop.google.com/access/\"");
+        }
+    }
+}
---
+++ ConsoleShortcuts/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleShortcuts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Process.Start("chrome.exe", "--app=\"https://remotedesktop.google.com/access/\"");
+        }
+    }
+}
